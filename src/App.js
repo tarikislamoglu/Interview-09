8aa77b5eb1a5d54e8014995a641b770aa2b41462
Tarık İslamoğlu <tarikislamoglu8@gmail.com>
2025-03-25 12:51:11 +0300

    tryagain

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [isSmall, setIsSmall] = useState(true);
+  const [buttonWidth, setButtonWidth] = useState(100);
+
+  const handleClick = () => {
+    setIsSmall((prev) => !prev);
+  };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setButtonWidth((prev) => {
+        if (!isSmall && prev > 100) {
+          return prev - 50;
+        } else if (isSmall && prev < 250) {
+          return prev + 50;
+        }
+        return prev;
+      });
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [isSmall]);
+
+  return (
+    <div className="flex justify-center mt-[30px] w-full">
+      <GrowingButton
+        handleClick={handleClick}
+        isSmall={isSmall}
+        buttonWidth={buttonWidth}
+      />
+    </div>
+  );
+}
+
+const GrowingButton = ({ handleClick, isSmall, buttonWidth }) => {
+  return (
+    <button
+      onClick={handleClick}
+      className={`border-2  py-2 w-[${buttonWidth}px]  bg-blue-400`}
+    >
+      {isSmall ? "Büyüt" : "Küçült"}
+    </button>
+  );
+};
+
+export default App;
